9096e9193f8e2c96f9f4d8b46cb73c5e1c987da8
Sithum Bopitiya <sithumsandeepap@gmail.com>
2023-10-13 06:41:16 +0530

    Clear the title and get the name

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const cheerio = require("cheerio");
 const { promises } = require("dns");
 
 const wikicfp = "http://www.wikicfp.com/cfp/rss?cat=";
+const googleScholar =
+  "https://scholar.google.com/citations?hl=en&view_op=search_venues&vq=";
 const categories = [
   "computer science",
   "security",
@@ -44,11 +46,13 @@ const parse = (xml) => {
     fetch(item.link).then((page) => {
       const pageData = scrape(page);
       const { title, link, description } = item;
+      const name = clearTitle(title);
       return {
         title,
         link,
         description,
         ...pageData,
+        name,
       };
     })
   );
@@ -106,6 +110,54 @@ const scrape = (page) => {
     categories,
     link,
   };
+};
+
+// Example titles
+// ESCC--EI 2024 : 2024 the 6th European Symposium on Computer and Communications (ESCC 2024)
+// ICCAI--EI 2024 : 2024 10th International Conference on Computing and Artificial Intelligence (ICCAI 2024)
+// ICDIP 2024 : 2024 16th International Conference on Digital Image Processing (ICDIP 2024)
+// ICICSE 2024 : IEEE--2024 the 4th International Conference on Information Communication and Software Engineering (ICICSE 2024)
+// ICBDC 2024 : 2024 9th International Conference on Big Data and Computing (ICBDC 2024)
+// RCIS 2024 : 18th Research Challenges in Information Science
+// SEAI  2024 : 2024 4th IEEE International Conference on Software Engineering and Artificial Intelligence (SEAI 2024)
+// IWPR 2024 : 2024 9th International Workshop on Pattern Recognition (IWPR 2024)
+// WCSE--EI 2024 : 2024 The 14th International Workshop on Computer Science and Engineering (WCSE 2024)
+// ICFCC--EI 2024 : 2024 The 16th International Conference on Future Computer and Communication (ICFCC 2024)
+// remove everything till the name of the conference and remove everything name of the conference
+// Example: ICBDC 2024 : 2024 9th International Conference on Big Data and Computing (ICBDC 2024) -> International Conference on Big Data and Computing
+// Example: SEAI  2024 : 2024 4th IEEE International Conference on Software Engineering and Artificial Intelligence (SEAI 2024) -> IEEE International Conference on Software Engineering and Artificial Intelligence
+// Example: WCSE--EI 2024 : 2024 The 14th International Workshop on Computer Science and Engineering (WCSE 2024) -> International Workshop on Computer Science and Engineering
+
+const clearTitle = (title) => {
+  const colon = title.split(":");
+  let name = colon[colon.length - 1].trim();
+  const regex = /(\d{4})\s(.*)\((.*)\)/;
+  const match = name.match(regex);
+  if (match) {
+    name = match[2].trim();
+  }
+  // 2nd International Conference on Automation and Engineering
+  // 8th International Conference on Networks and Communications
+  // International Japan-Africa Conference on Electronics, Communications and Computations 2023
+  // 17th IADIS International Conference Information Systems 2024
+  // 22nd International Conference e-Society 2024
+  // 20th International Conference Mobile Learning 2024
+  // 17th IADIS International Conference Information Systems 2024
+  // 22nd International Conference e-Society 2024
+  // 27th Conference on Innovation in Clouds, Internet and Networks
+  const regex2 = /(\d{1,2})(st|nd|rd|th)\s(.*)/;
+  const match2 = name.match(regex2);
+  if (match2) {
+    name = match2[3].trim();
+  }
+  // remove the year
+  const regex3 = /(.*)\s(\d{4})/;
+  const match3 = name.match(regex3);
+  if (match3) {
+    name = match3[1].trim();
+  }
+
+  return name;
 };
 
 const main = () => {
